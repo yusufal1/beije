e0c248479786c0a95962b1df1c8eae5c4ef77c7e
yusufal1 <yusufal5558@gmail.com>
2024-06-22 17:54:56 +0300

    completed custom packet

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,27 +1,25 @@
 "use client"
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import Logo from '../../../public/logo.svg';
 import Basket from '../../../public/basket.svg';
 import Profile from '../../../public/profile.svg';
+import Hamburger from '../../../public/hamburger.svg';
+
 
 const Header = () => {
-  const headerRef = useRef(null);
-
-  useEffect(() => {
-    const headerHeight = headerRef.current.offsetHeight;
-    document.body.style.paddingTop = `${headerHeight}px`;
-  }, []);
+  const [isOpen, setOpen] = useState(false)
+ 
 
   return (
-    <header ref={headerRef} className='flex flex-col fixed top-0 left-auto right-0 shrink-0 z-50 bg-primary justify-center items-center w-full p-6'>
+    <header className='flex flex-col fixed top-0 left-auto right-0 shrink-0 z-50 bg-primary justify-center items-center w-full p-6'>
       <div className='w-full mx-auto flex flex-row gap-6 lg:max-w-[1152px]'>
         <div className='flex items-center w-[172px]'>
           <a href="/">
             <Image src={Logo} width={56.39}/>
           </a>
         </div>
-        <nav className='flex gap-8 flex-grow flex-nowrap'>
+        <nav className='md:flex hidden gap-8 flex-grow flex-nowrap'>
           <div className='inline-flex relative'>
             <a href='/' className='text-[#000000e6] text-nowrap'>Ürünler</a>
           </div>
@@ -47,6 +45,11 @@ const Header = () => {
           <a href="/">
             <span className='relative inline-flex align-middle'>
               <Image src={Profile} width={24}/>
+            </span>
+          </a>
+          <a href="/">
+            <span className='relative inline-flex align-middle'>
+            <Image src={Hamburger} width={24}/>
             </span>
           </a>
         </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        "primary": "#F9F5F2",
+        "primary": "rgb(245, 245, 245)",
         "secondary": "#343131"
       }
     },
